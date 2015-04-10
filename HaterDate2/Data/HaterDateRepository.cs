b8f3437b445a7923e9b7916e000e48c4c76e815f
adamsheaffer <adam.e.sheaffer@gmail.com>
2015-04-10 14:26:33 -0500

    created empty repository

---
+++ HaterDate2/Data/HaterDateRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HaterDate2.Data
+{
+    public class HaterDateRepository : IHaterDateRepository
+    {
+        public HaterDateRepository()
+        {
+
+        }
+
+        public IQueryable<Models.Hate> GetAllQuestions()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Models.Profile GetProfileById(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<Models.Hate> GetUserHates(string userId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
