1bb5c5222d28ceacd7d1a8d023ef74f12b2f8848
adamsheaffer <adam.e.sheaffer@gmail.com>
2015-04-10 13:29:12 -0500

    added Hate class

---
+++ HaterDate2/Models/Hate.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HaterDate2.Models
+{
+    public class Hate
+    {
+        public int id { get; set; }
+        public string Description { get; set; }
+    }
+}
